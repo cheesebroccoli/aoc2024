e67586da857b345ed7c101136077ba7119de55d2
cheesebroccoli <122124161+cheesebroccoli@users.noreply.github.com>
2024-12-01 18:17:45 -0500

    Update code.java

--- 1/code.java
+++ 1/code.java
@@ -1,20 +1,26 @@
+//I am making an assumption that nextLine does not split my spaces so I'm doing it manually
 import java.io.File;  // Import the File class
 import java.io.FileNotFoundException;  // Import this class to handle errors
-import java.util.Scanner; // Import the Scanner class to read text files
+import java.util.*;
 
-public class ReadFile {
+public class dayOne {
   public static void main(String[] args) {
+    List<Integer> listOne = new ArrayList<>();
+    List<Integer> listOne = new ArrayList<>();
     try {
       File myObj = new File("input.txt");
-      Scanner myReader = new Scanner(myObj);
-      while (myReader.hasNextLine()) {
+      Scanner input = new Scanner(myObj);
+      while (input.hasNextLine()) {
         String data = myReader.nextLine();
-        System.out.println(data);
+        
       }
+      
       myReader.close();
     } catch (FileNotFoundException e) {
       System.out.println("An error occurred.");
       e.printStackTrace();
     }
+    public static void split(String data){
+      
   }
 }
